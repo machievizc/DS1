8fef1ce8cfca8fc60ce08293749b82922184ed08
mikael18 <machievizc@hotmail.com>
2020-03-21 08:22:28 -0300

    aula 19 / 03 / 2020

--- exemplo/phphphp.php
+++
@@ -1,6 +0,0 @@
-<?php
-                                   
-                                   
-                                   
-                                   print_r(file("test.txt"));
-                         ?>
